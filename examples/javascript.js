6959cbb116ccbbea3b67d791f4e17653b12c4c65
barrydevp <barrydevp@gmail.com>
2024-03-28 15:07:56 -0400

    feat: init repository
    - Add language configs
    - Add example codes

---
+++ examples/javascript.js
@@ -0,0 +1,19 @@
+console.log("")
+console.log("hello", "world", "frombro")
+console.log(
+    '',
+    'hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh',
+    'form',
+    'the',
+    'vietnam',
+    'you know',
+    'the best',
+    'country in the world',
+    'I love my country',
+    'very much',
+    'there is a lot of beautiful',
+    'places',
+    'nice people',
+    'and delicious food'
+)
+
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+    println!("This is a Rust program.");
+    println!("It doesn't do much yet, but it will soon!");
+    println!("Exiting now.");
+    std::process::exit(0);
+}
